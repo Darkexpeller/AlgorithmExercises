d8decc4c01cb412e34a4ba5f7a50cd66c1bfcdcb
Darkexpeller <wcultivate@gmail.com>
2026-03-13 21:24:39 +0800

    3-13

--- 11-maxArea.cpp
+++ 11-maxArea.cpp
@@ -17,9 +17,33 @@
 #include <algorithm>
 #include<unordered_set>
 using namespace std;
+/*思路：
+    暴力组合有C(2,n)个，双指针初始在左右两端，每次将短板往里移。迭代会排除掉n-1 n-2 n-3....1个不优状态
+    这样当左右指针相遇时刚好排除掉了C(2,n)个状态。该贪心策略可行。
+*/
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
+        if(height.size()==0)
+            return 0;
+        else if(height.size()==1)
+            return height[0];
+        int left = 0;
+        int right= height.size() - 1;
+        int maxWater=0 ;
+        while(left!=right){
+            maxWater = max((min(height[left], height[right])) * (right - left),maxWater);
+            if(height[left]<=height[right]) //将短板往里面移。
+                ++left;
+            else
+                --right;
+        }
+        return maxWater;
     }
 };
+
+int main(){
+    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    Solution s;
+    printf("%d\n", s.maxArea(height));
+}

51fcdbd0d2f9123bcc23ee74cc1ddf294fba4026
Darkexpeller <wcultivate@gmail.com>
2026-03-16 22:06:08 +0800

    3-16

--- 101-isSymmetric.cpp
+++ 101-isSymmetric.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
+#include<queue>
 using namespace std;
 struct TreeNode
 {
@@ -17,5 +18,18 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         
+
+    }
+    TreeNode *invertTree(TreeNode* root){
+        if(root!=nullptr){
+            TreeNode *temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+        }
+        if(root->left!=nullptr)
+            invertTree(root->left);
+        if(root->right!=nullptr)
+            invertTree(root->right);
+        return root;
     }
 };
